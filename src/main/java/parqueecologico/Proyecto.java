9c699b1f102e34bc8abfff5dea5a8ec825d7716c
JoseQ29 <jose.quintanilla@est.fi.uncoma.edu.ar>
2026-02-08 14:21:52 -0300

    Primer commit

---
+++ src/main/java/parqueecologico/Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package parqueecologico;
+
+/**
+ *
+ * @author Razor-PC V.3
+ */
+public class Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
