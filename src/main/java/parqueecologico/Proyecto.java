c5e193b50b0154c5fb759f5eb1d455de1aa8ccb1
JoseQ29 <jose.quintanilla@est.fi.uncoma.edu.ar>
2026-02-08 14:29:38 -0300

    prueba de cambio de nombre

--- src/main/java/parqueecologico/Proyecto.java
+++ src/main/java/parqueecologico/Proyecto.java
@@ -11,6 +11,6 @@ package parqueecologico;
 public class Proyecto {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo!!");
     }
 }
